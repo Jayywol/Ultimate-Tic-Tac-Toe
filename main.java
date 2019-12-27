ef72e6293aa95234a9bc20ad7dee2435589534f6
jayywol <ma_alexandre@outlook.com>
2019-12-27 23:59:34 +0100

    Add class Case

--- main.java
+++ main.java
@@ -2,29 +2,43 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-class Player {
-
-    public static void main(String args[]) {
+class Player
+{
+    public static void main(String args[])
+    {
         Scanner in = new Scanner(System.in);
-
-        // game loop
-        while (true) {
+        while (true)
+        {
             int opponentRow = in.nextInt();
             int opponentCol = in.nextInt();
             int validActionCount = in.nextInt();
-            for (int i = 0; i < validActionCount; i++) {
+            for (int i = 0; i < validActionCount; i++)
+            {
                 int row = in.nextInt();
                 int col = in.nextInt();
             }
-
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
-
             System.out.println("0 0");
         }
+    }
+}
+
+class Case
+{
+    private int x, y;
+
+    public Case(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
     }
 }
